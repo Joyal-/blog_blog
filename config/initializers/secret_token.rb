7d607a3aa75477303adbc94e72c72cb4c7f97609
Joyal Hines <Jhines@chicagotechacademy.org>
2013-12-19 13:10:10 -0600

    Revert "7:43 commit"
    This reverts commit 24e565356f05d0bae662adb805c6fe945d895fdd.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BlogBlog::Application.config.secret_token = '17d94e56e3a0296d4924b8d20acf0347441259141cc4957b36ce508f040f928297031dcd3a1aa829cf1a75ba17a911d1774fcda4e9218977ae4fda451a421698'
